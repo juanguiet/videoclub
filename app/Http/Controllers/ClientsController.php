bbc86a250ad97ea3e1e1bd025245411238241650
juanguillermoecheverritapias@gmail.com <juanguillermoecheverritapias@gmail.com>
2022-08-03 18:49:42 -0500

    El ítem de clientes ya registra los datos cargados a través del archivo de excel cargado.

--- app/Http/Controllers/ClientsController.php
+++ app/Http/Controllers/ClientsController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ClientsRequest;
+use App\Imports\ClientDataImport;
 use App\Models\ClienteDato;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\View;
+use Maatwebsite\Excel\Facades\Excel;
 
 class ClientsController extends Controller
 {
@@ -26,13 +28,21 @@ class ClientsController extends Controller
     {
         $fl_clients_upload = $request->file('formFileUploadClients');
 
-        dd($fl_clients_upload);
+        Excel::import(new ClientDataImport, $fl_clients_upload);
+
+        return response()
+                ->json(
+                    [
+                        'status' => 'ok',
+                        'action' => 'imported',
+                    ], 200
+                );
     }
 
     public function get_clients()
     {
         $clients = ClienteDato::GetInfo()->get();
-        $view = count($clients) > 0 ? 'pages.clients.components.lists.clients-register' : 'vendor.messages.empty-data';
+        $view = count($clients) > 0 ? 'pages.clients.components.lists.clients-registers' : 'vendor.messages.empty-data';
 
         return response()
                 ->json([
